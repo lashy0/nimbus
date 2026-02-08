710ed5e6b95edbe81cb8c85d84f1159f9e9122c6
Daniil Alekseev <lashy.danya.work@gmail.com>
2026-02-08 21:14:53 +0300

    chore: track binary assets with git-lfs

--- main/main.c
+++ main/main.c
@@ -7,12 +7,12 @@
 #include "esp_lcd_panel_ops.h"
 #include "esp_log.h"
 #include "esp_lvgl_port.h"
+#include "esp_spiffs.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "iot_button.h"
 #include "lvgl.h"
-#include "screens.h"
 #include "ui.h"
 
 static const char* TAG = "main";
@@ -20,43 +20,78 @@ static const char* TAG = "main";
 #define BUTTON_PREV_GPIO GPIO_NUM_0
 #define BUTTON_NEXT_GPIO GPIO_NUM_35
 
-static const enum ScreensEnum screen_list[] = {
-    SCREEN_ID_IAQ,
-    SCREEN_ID_TEMP,
-    SCREEN_ID_HUM,
-};
-static int       current_screen_index = 0;
-static const int screen_count = sizeof(screen_list) / sizeof(screen_list[0]);
-
 static void ui_tick_timer_cb(lv_timer_t* t) { ui_tick(); }
+
+// Test ui
+static void sensor_timer_cb(lv_timer_t* t)
+{
+    static int counter = 0;
+    counter++;
+    
+    if (lvgl_port_lock(10))
+    {
+        ui_update_iaq((counter * 7) % 500);
+        ui_update_temp(-10 + (counter % 50));
+        ui_update_hum(counter % 100);
+        ui_update_battery(100 - (counter % 100), (counter / 10) % 2);
+        lvgl_port_unlock();
+    }
+}
 
 static void button_press_cb(void* arg, void* data)
 {
-    int  btn_id = (int)data;
-    bool screen_changed = false;
+    int btn_id = (int)data;
 
-    if (btn_id == 0)
+    if (lvgl_port_lock(-1))
     {
-        ESP_LOGI(TAG, "Button PREV pressed");
-        current_screen_index--;
-        if (current_screen_index < 0) current_screen_index = screen_count - 1;
-        screen_changed = true;
+        if (btn_id == 0)
+        {
+            ESP_LOGI(TAG, "Button PREV pressed");
+            ui_switch_prev();
+        }
+        else
+        {
+            ESP_LOGI(TAG, "Button NEXT pressed");
+            ui_switch_next();
+        }
+
+        lvgl_port_unlock();
+    }
+}
+
+void mount_spiffs(void)
+{
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/spiffs", .partition_label = "storage", .max_files = 5, .format_if_mount_failed = true};
+
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK)
+    {
+        if (ret == ESP_FAIL)
+        {
+            ESP_LOGE("SPIFFS", "Failed to mount or format filesystem");
+        }
+        else if (ret == ESP_ERR_NOT_FOUND)
+        {
+            ESP_LOGE("SPIFFS", "Failed to find SPIFFS partition");
+        }
+        else
+        {
+            ESP_LOGE("SPIFFS", "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(conf.partition_label, &total, &used);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE("SPIFFS", "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
     }
     else
     {
-        ESP_LOGI(TAG, "Button NEXT pressed");
-        current_screen_index++;
-        if (current_screen_index >= screen_count) current_screen_index = 0;
-        screen_changed = true;
-    }
-
-    if (screen_changed)
-    {
-        if (lvgl_port_lock(0))
-        {
-            loadScreen(screen_list[current_screen_index]);
-            lvgl_port_unlock();
-        }
+        ESP_LOGI("SPIFFS", "Partition size: total: %d, used: %d", total, used);
     }
 }
 
@@ -78,6 +113,8 @@ void app_main(void)
     ESP_ERROR_CHECK(backlight_init(&bl_config, &bl_handle));
     ESP_ERROR_CHECK(backlight_set_brightness(&bl_handle, 100));
 
+    mount_spiffs();
+
     ESP_LOGI(TAG, "Init LVGL Port...");
     const lvgl_port_cfg_t lvgl_cfg = {
         .task_priority = 4,
@@ -92,7 +129,7 @@ void app_main(void)
     const lvgl_port_display_cfg_t disp_cfg = {
         .io_handle = disp_hw.io_handle,
         .panel_handle = disp_hw.panel_handle,
-        .buffer_size = HEIGHT * 20,
+        .buffer_size = WIDTH * HEIGHT / 10,
         .double_buffer = true,
         .hres = WIDTH,
         .vres = HEIGHT,
@@ -111,7 +148,7 @@ void app_main(void)
             },
     };
 
-    lv_disp_t* disp = lvgl_port_add_disp(&disp_cfg);
+    lvgl_port_add_disp(&disp_cfg);
 
     // Buttons
     const button_config_t btn_cfg = {
@@ -161,8 +198,10 @@ void app_main(void)
     if (lvgl_port_lock(0))
     {
         ui_init();
-        lv_timer_create(ui_tick_timer_cb, 5, NULL);
 
+        lv_timer_create(sensor_timer_cb, 2000, NULL);
+
+        lv_timer_create(ui_tick_timer_cb, 5, NULL);
         lvgl_port_unlock();
     }
 
